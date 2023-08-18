8b420ab01caa308e518bc5b27e1e791ce37d0931
Bananka25 <datkageo@mail.ru>
2023-08-18 13:51:09 +0400

    done

---
+++ work/Homework_v4.js
@@ -0,0 +1,13 @@
+let car = {
+  brand: String,
+  model: String,
+  year: Number,
+  isSportCar: Boolean
+}
+
+car.brand = "Toyota";
+car.model = "Supra";
+car.year = 2018;
+car.isSportCar = true;
+
+console.log(car);
